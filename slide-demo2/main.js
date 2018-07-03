5c67d7cde07ce0d143dc4db4aaeb8024d1eb5bcb
qikke <qikke1995@126.com>
2018-07-03 11:45:02 +0800

    demo2优化为面向对象的写法

--- slide-demo2/main.js
+++ slide-demo2/main.js
@@ -1,36 +1,60 @@
-! function () {
-  var imgs = $('.imgWrap > img')
-  var imgWidth = imgs.eq(1).width()
-  var n = 0
-  var timer = null
+class Slide {
+  constructor(options) {
+    this.defaultOptions = {
+      element: '',
+      speed: 1000,
+    }
+    this.options = Object.assign({}, this.defaultOptions, options)
+    this.imgWrap = $(`${this.options.element}`)
+    this.imgs = this.imgWrap.children('img')
+    this.imgWidth = this.imgs.eq(1).width()
+    this.n = 0
+    this.timer = null
 
-  $('.imgWrap').append(imgs.first().clone())
-  $('.imgWrap').prepend(imgs.last().clone())
-
-  $('.imgWrap').css({
-    left: '-' + imgWidth + 'px'
-  })
-
-  start()
-
-  function start() {
-    timer = setInterval(() => {
-      changeTo(++n)
-    }, 1000)
+    this.checkOptions().init().start()
   }
 
-  function changeTo(index) {
-    $('.imgWrap').animate({
-        left: '-' + imgWidth * n + 'px'
+  checkOptions() {
+    if (!this.options.element) {
+      throw new Error('element is required!')
+    }
+    return this
+  }
+
+  init() {
+    this.imgWrap.append(this.imgs.first().clone())
+    this.imgWrap.prepend(this.imgs.last().clone())
+    this.imgWrap.css({
+      left: '-' + this.imgWidth + 'px',
+    })
+    return this
+  }
+
+  start() {
+    this.timer = setInterval(() => {
+      this.changeTo(++this.n)
+    }, this.options.speed)
+    return this
+  }
+
+  changeTo(index) {
+    this.imgWrap.animate({
+        left: '-' + this.imgWidth * this.n + 'px'
       },
       500,
       () => {
-        if (++index == 4) {
-          n = 0
-          $('.imgWrap').css({
+        if (++index == this.imgs.length + 1) {
+          this.n = 0
+          this.imgWrap.css({
             left: '0'
           })
         }
       })
   }
+}
+
+! function () {
+  new Slide({
+    element: '.imgWrap'
+  })
 }.call()
